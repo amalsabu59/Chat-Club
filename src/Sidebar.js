41b45dd8c7a19e9cd621d6b371fb21fa42749ecd
amalsabu59 <amalsabu59@gmail.com>
2022-02-01 17:13:48 +0530

    sidebar search and icons added using material ui

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Sidebar.css'
-
+import MessageIcon from '@mui/icons-material/Message';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import { IconButton ,Avatar} from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
 function Sidebar() {
-  return <div className='sidebar'>
-      <h1>i am a sidebar</h1>
-  </div>
+    return <div className='sidebar'>
+
+        <div className="sidebar_header">
+            <Avatar src='https://avatars.githubusercontent.com/u/92331948?v=4' />
+            <div className="sidebar_headerRight">
+                <IconButton>
+                    <DonutLargeIcon />
+                </IconButton>
+                <IconButton>
+                    <MessageIcon />
+                </IconButton>
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+            </div>
+        </div>
+        <div className="sidebar_search">
+            <div className="sidebar_searchContainer">
+            <SearchIcon />
+            <input placeholder='search or start new chat' type="text" />
+            </div>
+        </div>
+    </div>
 }
 
 export default Sidebar;
+  
