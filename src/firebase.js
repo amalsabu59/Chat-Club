06364bc4f12c4d4e8daf3c35db9a354d6065c4d8
amalsabu59 <amalsabu59@gmail.com>
2022-02-01 15:28:37 +0530

    side bar and chatbox added

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoolN_mqGw5XUcXi_T4GEf1KvAmM1jaD8",
+    authDomain: "chathouse-79d94.firebaseapp.com",
+    projectId: "chathouse-79d94",
+    storageBucket: "chathouse-79d94.appspot.com",
+    messagingSenderId: "665269136753",
+    appId: "1:665269136753:web:d8afdbc4d41414a12a9ad0"
+  };
