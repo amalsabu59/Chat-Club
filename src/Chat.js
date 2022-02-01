756a1038ed5970157682c273d680ff1ec17eb193
amalsabu59 <amalsabu59@gmail.com>
2022-02-01 20:36:04 +0530

    chat body and chat header ui updated

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,53 @@
 import React from 'react';
 import './Chat.css'
-
+import { IconButton ,Avatar} from '@mui/material';
+import YoutubeSearchedForIcon from '@mui/icons-material/YoutubeSearchedFor';
+import AttachmentIcon from '@mui/icons-material/Attachment';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 function Chat() {
   return <div className='chat'>
-      <h1>I am the chat</h1>
-  </div>;
+        <div className="chat_header">
+            <Avatar />
+            <div className="chat_headerInfo">
+                <h3>Room name</h3>
+                <p>last seen 12:50 pm</p>
+            </div>
+            <div className="chat_headerRight">
+                <IconButton>
+                    <YoutubeSearchedForIcon />
+                </IconButton>
+                <IconButton>
+                    <AttachmentIcon />
+                </IconButton>
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+            </div>
+        </div>
+        <div className="chat_body">
+            <p className='chat_message'>
+                <span className='chat_name'>Amal</span>
+                this is a message
+                <span className='chat_timestamp'>{new
+                Date().toUTCString()}</span>
+
+            </p>
+            <p className='chat_message'>
+                <span className='chat_name'>Amal</span>
+                this is a message
+                <span className='chat_timestamp'>{new
+                Date().toUTCString()}</span>
+
+            </p>
+            <p className='chat_reciever chat_message'>
+                <span className='chat_name'>Amal</span>
+                this is a message
+                <span className='chat_timestamp'>{new
+                Date().toUTCString()}</span>
+
+            </p>
+        </div>
+    </div>;
 }
 
 export default Chat;
